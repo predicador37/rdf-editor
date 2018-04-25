6d0231e50849299737df0f851d324153e13433ac
Miguel Expósito <exposito_m@cantabria.es>
2018-04-26 00:02:56 +0200

    Configurado vuex e inicializado con grafo de ejemplo

--- src/components/Model.vue
+++ src/components/Model.vue
@@ -1,6 +1,7 @@
 <template>
+  <div>
   <v-tabs icons-and-text>
-    <v-tabs-slider color="yellow"></v-tabs-slider>
+    <v-tabs-slider color="primary"></v-tabs-slider>
     <v-tab href="#tab-1">
       Clases
       <v-icon>mdi-file-tree</v-icon>
@@ -13,27 +14,69 @@
       Atributos
       <v-icon>mdi-format-list-bulleted-type</v-icon>
     </v-tab>
-    <v-tab-item
-      v-for="i in 3"
-      :key="i"
-      :id="'tab-' + i"
-    >
+    <v-tab-item key="1" id="tab-1">
+      <v-container grid-list-md >
+        <v-layout row wrap>
+          <v-flex xs6>
+      <v-card flat>
+        <v-card-text>
+          <v-treeview :items="items"></v-treeview>
+        </v-card-text>
+      </v-card>
+          </v-flex>
+          <v-flex xs6>
+            <v-card flat>
+              <v-card-text>
+                <v-treeview :items="items"></v-treeview>
+                {{dataset}}
+              </v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-tab-item>
+    <v-tab-item key="2" id="tab-2">
+      <v-card flat>
+        <v-card-text>{{ text }}</v-card-text>
+      </v-card>
+    </v-tab-item>
+    <v-tab-item key="3" id="tab-3">
       <v-card flat>
         <v-card-text>{{ text }}</v-card-text>
       </v-card>
     </v-tab-item>
   </v-tabs>
+  </div>
 </template>
 
 <script>
-    export default {
-      data () {
-        return {
-          text: 'prueba'
+  import Treeview from '@/components/Treeview'
+  import {mapState} from 'vuex'
+  export default {
+    computed: mapState(['dataset', 'initialState']),
+    components: {
+      'v-treeview': Treeview
+    },
+    data () {
+      return {
+        text: 'prueba',
+        items: {
+          name: 'Thing',
+          children: [
+            {
+              name: 'Organization'
+            },
+            {name: 'Address',
+              children: [{
+                name: 'Thoroughfare type'
+              },
+              {name: 'Thoroughfare number'}]}
+          ]
         }
-      },
-      name: 'Model'
-    }
+      }
+    },
+    name: 'Model'
+  }
 </script>
 
 <style scoped>
