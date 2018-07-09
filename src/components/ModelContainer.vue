8885d69786874209ea0d842f4cf1f4e04d2722f3
Miguel Expósito <exposito_m@cantabria.es>
2018-07-10 00:29:58 +0200

    Corregido bug al recuperar recursos de tipo clase

--- src/components/ModelContainer.vue
+++ src/components/ModelContainer.vue
@@ -106,13 +106,14 @@
       },
       handleAddResource (resourceName, resourceType) {
         this.addResource({'subject': this.baseUrl + resourceName, 'predicate': this.rdfConstructs.rdf_type.value, 'object': resourceType})
-        resourceType = resourceType.split('#')[1].toLowerCase()
+        // resourceType = resourceType.split('#')[1].toLowerCase()
         // using a string concatenation as parameter: 'add'+type to call the methods dynamically
         // this['add' + resourceType.charAt(0).toUpperCase() + resourceType.slice(1)](this.baseUrl + resourceName) // addClass
         // not necessary... a generic resource can be added. But getting classes is the result of joining two datasets...
         // how to generalize this?
-        this[resourceType.match('[ch|sh|s|x|z]$') ? 'get' + resourceType.charAt(0).toUpperCase() + resourceType.slice(1) + 'es' : 'get' + resourceType.charAt(0).toUpperCase() + resourceType.slice(1) + 's']() // getClasses
+        // this[resourceType.match('[ch|sh|s|x|z]$') ? 'get' + resourceType.charAt(0).toUpperCase() + resourceType.slice(1) + 'es' : 'get' + resourceType.charAt(0).toUpperCase() + resourceType.slice(1) + 's']() // getClasses
         // this.getClasses()
+        this.getResources({'predicate': this.rdfConstructs.rdf_type.value, 'object': resourceType})
       },
       handleAddLiteralProperty (triple) {
         this.addClassLiteralProperty({subject: triple.resource, predicate: this.rdfConstructs[triple.propertyName].value, object: triple.literal})
