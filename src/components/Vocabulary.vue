8b6d3d9c09bb5cd2ce72165354331e918f0fecb9
Miguel Expósito <exposito_m@cantabria.es>
2018-08-17 00:11:12 +0200

    Finalizada funcionalidad de cargar y descargar vocabularios. Pendiente de solucionar bug de estado: recordar al salir y volver de la sección el estado de cada vocabulario

--- src/components/Vocabulary.vue
+++ src/components/Vocabulary.vue
@@ -11,7 +11,7 @@
           <v-card-text>
             <p>Desde aquí puedes precargar algunos de los vocabularios más utilizados en tu grafo para poder utilizarlos posteriormente en tus consultas SPARQL.</p>
 
-              <v-switch v-for="value in vocabularies" v-model="selectedVocabulary" @change="changeVocabularyState"  :label="value.name" :false-value="{delN3: value.url}" :true-value="{addN3: value.url}"></v-switch>
+              <v-switch v-for="value in vocabularies" v-model="selectedVocabulary" :key="value.name" @change="changeVocabularyState"  :label="value.name" :false-value="{method: 'delN3', url: value.url}" :true-value="{method: 'addN3', url: value.url}"></v-switch>
 
           </v-card-text>
           <v-snackbar
@@ -35,6 +35,7 @@
 
 <script>
   import Vocabularies from '../utils/Vocabularies'
+  import {mapActions} from 'vuex'
   export default {
     name: 'vocabulary',
     data () {
@@ -45,13 +46,21 @@
         snackbarMessage: 'Vocabulario importado'
       }
     },
-    methods: {
+    methods: { ...mapActions(['addN3', 'delN3']),
       changeVocabularyState () {
         console.log(JSON.stringify(this.selectedVocabulary))
+        this.$http.get(this.selectedVocabulary.url).then(response => {
+          // get body data
+          console.log(this.selectedVocabulary.name)
+          console.log(response.body)
+          this[this.selectedVocabulary.method]({'content': response.body, 'store': 'n3store'})
+        }, response => {
+          // error callback
+        })
+      }
         // todo implement enableVocabulary and disableVocabulary
         // add vocabulary to graph from URL
         // delete vocabulary from graph
-      }
     }
   }
 </script>
